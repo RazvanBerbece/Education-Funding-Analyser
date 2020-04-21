ebca893d974116d545778b62bd9290bddcdf3d7e
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2020-04-21 18:26:36 +0100

    Linear Regression almost fully implemented. Needs to be calculated using input. Changes to the dataset.

--- predict.py
+++ predict.py
@@ -6,6 +6,11 @@ from loggingoutput import writeToLog
 ## Predict Methodology ##
 from sklearn import linear_model
 import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 logfile = 'log.txt'
 inputs = []
@@ -27,13 +32,47 @@ def printInput(input):
 ## Retrieving input list from JS ##
 @eel.expose
 def getInputList():
-    """ Gets the input list fro, the JS side, and prints using a callback function """
+    """ Gets the input list from the JS side, and prints using a callback function """
     writeToLog("Called getInputList() in Python ...", logfile)
     inputs = eel.logPythonInput()(printInput)
 
+def assessModel(regressor, predictors, predicted):
+    """ Assesses the efficiency of the calculated model """
+    #To retrieve the intercept:
+    print(regressor.intercept_)
+    #For retrieving the slope:
+    print(regressor.coef_)
+    print(regressor.score(predictors, predicted))
+    writeToLog("Current score for LR : " + str(regressor.score(predictors, predicted)), logfile)
+
+def modelSummary(dataset):
+    """ Prints the model's summary table """
+    X = np.column_stack((dataset['govFunds'], dataset['primaryEnroll']))
+    y = dataset['illiteracyNo']
+    X2 = sm.add_constant(X)
+    est = sm.OLS(y, X2)
+    est2 = est.fit()
+    print(est2.summary())
+
 def main():
-    data = pd.read_csv("Dataset/pythstats.csv")
-    print(data)
+    
+    ## Loading the CSV file which has the Education Statistics ##
+    dataset = pd.read_csv("Dataset/pythstats.csv")
+    print(dataset)
+
+    Xm = dataset.drop(['country', 'illiteracyNo'], axis=1)
+    y = dataset['illiteracyNo'].values.reshape(-1,1)
+
+    ## Spliting the dataset into Training set and Test Set ##
+    x_train, x_test, y_train, y_test = train_test_split(Xm, y, test_size= 0.2, random_state=0)
+
+    ## Training the model ##
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)
+
+    ## Assessing the model ##
+    modelSummary(dataset)
+    assessModel(regressor, Xm, y)   
 
 if __name__ == "__main__":
     main()
